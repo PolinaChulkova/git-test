8537e41c3dce39d8236a114f69ef88623eb4f7fd
Полина <shelkova1937@gmail.com>
2023-04-29 22:14:14 +0500

    third

--- src/main/java/com/example/gistest/repository/EntityRepository.java
+++ src/main/java/com/example/gistest/repository/EntityRepository.java
@@ -3,9 +3,11 @@ package com.example.gistest.repository;
 import com.example.gistest.entity.Entity;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import reactor.core.publisher.Flux;
 
 @Repository
+@Transactional
 public interface EntityRepository extends ReactiveCrudRepository<Entity, Long> {
 
     Flux<Entity> findAllByUserId(Long userId);
--- src/main/java/com/example/gistest/repository/UserRepository.java
+++ src/main/java/com/example/gistest/repository/UserRepository.java
@@ -4,9 +4,11 @@ import com.example.gistest.entity.User;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import reactor.core.publisher.Mono;
 
 @Repository
+@Transactional
 public interface UserRepository extends ReactiveCrudRepository<User, Long> {
     Mono<User> findByLogin(String login);
 }
